45ae1e2c06dc62690ff88d3e9d20efd55a9d45a1
sjklan4 <dpeltms4@gmail.com>
2023-11-02 23:35:49 +0900

    start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    public static void Main(string[] args) {
+        bool boolvariable = true;
+        int intvariable = 10;
+        float floatvariable = 3.4f;
+        char charvariable = 'a';
+
+        Console.WriteLine(boolvariable);
+        Console.WriteLine(intvariable);
+        Console.WriteLine(floatvariable);
+        Console.WriteLine(charvariable);
+    }
+}
